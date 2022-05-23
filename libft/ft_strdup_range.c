29b2dca2e41943b6af45505a64b68039da3ecd4b
MOOK <mukmookk@gmail.com>
2022-05-24 04:02:10 +0900

    🐛Fix: remove duplicate function ft_strjoin_range in ft_strdup_range.c

--- libft/ft_strdup_range.c
+++ libft/ft_strdup_range.c
@@ -1,30 +1,5 @@
 #include "libft.h"
 
-char	*ft_strjoin_range(char const *s1, char const *s2, int start, int end)
-{
-	char	*dst;
-	char	*temp;
-	int		n;
-	int		i;
-
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	n = ft_strlen(s1) + ft_strlen(s2) + 1;
-	dst = (char *)malloc(n * sizeof(char));
-	if (dst == NULL)
-		return (NULL);
-	temp = dst;
-	while (*s1)
-		*temp++ = *s1++;
-	i = start;
-	while (s2[i] && i < end)
-		*temp++ = s2[i++];
-	*temp = 0;
-	return (dst);
-}
-
-#include "libft.h"
-
 char	*ft_strdup_range(const char *s1, int start, int end)
 {
 	int		size;
