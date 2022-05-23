1a2f9fa6b7c15ffe7e4a3f6f8fe57b4cafbfb8da
MOOK <mukmookk@gmail.com>
2022-05-23 20:17:05 +0900

    🐛bug: Norm error-Assignment in control structure

--- libft/ft_malloc.c
+++ libft/ft_malloc.c
@@ -6,7 +6,7 @@
 /*   By: youngmki <youngmki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 18:59:46 by youngmki          #+#    #+#             */
-/*   Updated: 2022/05/23 18:59:50 by youngmki         ###   ########.fr       */
+/*   Updated: 2022/05/23 20:15:18 by youngmki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	*ft_malloc(size_t size)
 {
 	void	*mem;
 
-	if (!(mem = malloc(size)))
+	mem = malloc(size);
+	if (!mem)
 		return (NULL);
 	ft_bzero(mem, size);
 	return (mem);
