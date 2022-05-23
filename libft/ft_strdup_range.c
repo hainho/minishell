038ab0718d34f531b4608ba45e4d25d7f1f94ba7
hainho <hainho9704@gmail.com>
2022-05-24 06:59:28 +0900

    ✨feat: strdup range function 수정

--- libft/ft_strdup_range.c
+++ libft/ft_strdup_range.c
@@ -3,18 +3,19 @@
 char	*ft_strdup_range(const char *s1, int start, int end)
 {
 	int		size;
-	char	*temp;
 	char	*dst;
 	int		i;
 
 	size = ft_strlen(s1);
-	dst = (char *)malloc((size + 1) * sizeof(char));
-	if (dst == NULL || start < 0 || end > size)
+	if (start < 0 || end < 0 || start > size || end > size || start > end)
 		return (NULL);
-	temp = dst;
-	i = start;
-	while (i < end)
-		*temp++ = s1[i++];
-	*temp = 0;
+	size = end - start;
+	dst = (char *)malloc((size + 1) * sizeof(char));
+	if (dst == NULL)
+		return (NULL);
+	i = -1;
+	while (++i < size)
+		dst[i] = s1[i + start];
+	dst[i] = 0;
 	return (dst);
 }
