0dfb849741f9ce08dbddc544ffed461db566d8f9
hainho <hainho9704@gmail.com>
2022-05-24 06:22:15 +0900

    🐛bugfix: parse command line function가 마지막 문자열 누락하지 않도록 수정

--- src/parsing.c
+++ src/parsing.c
@@ -1,36 +1,5 @@
 #include "minishell.h"
 
-char	*parse_cmd_line(char *cmd_line)
-{
-	int     idx;
-	int		start_idx;
-	char	*parsed_line;
-	char	*join_line;
-	char	*temp;
-
-	parsed_line = (char *)malloc(sizeof(char));
-	parsed_line[0] = 0;
-	idx = -1;
-	start_idx = 0;
-	while (cmd_line[++idx])
-	{
-		if (cmd_line[idx] == '$')
-			join_line = parse_env(cmd_line, start_idx, &idx);
-		else if (cmd_line[idx] == '\'')
-			join_line = parse_quote(cmd_line, start_idx, &idx);
-		else if (cmd_line[idx] == '\"')
-			join_line = parse_double_quote(cmd_line, start_idx, &idx);
-		else
-			continue;
-		temp = ft_strjoin(parsed_line, join_line);
-		free(parsed_line);
-		free(join_line);
-		parsed_line = temp;
-		start_idx = idx + 1;
-	}
-	return (parsed_line);
-}
-
 static char	*parse_env(char *cmd_line, int start_idx, int *idx)
 {
 	char	*parsed_line;
@@ -99,6 +68,44 @@ static char	*parse_double_quote(char *cmd_line, int start_idx, int *idx)
 		(*idx)++;
 	}
 	join_line = ft_strdup_range(cmd_line, start_idx, *idx);
+	temp = ft_strjoin(parsed_line, join_line);
+	free(parsed_line);
+	free(join_line);
+	parsed_line = temp;
+	return (parsed_line);
+}
+
+char	*parse_cmd_line(char *cmd_line)
+{
+	int     idx;
+	int		start_idx;
+	char	*parsed_line;
+	char	*join_line;
+	char	*temp;
+
+	parsed_line = (char *)malloc(sizeof(char));
+	parsed_line[0] = 0;
+	idx = -1;
+	start_idx = 0;
+	while (cmd_line[++idx])
+	{
+		if (cmd_line[idx] == '$')
+			join_line = parse_env(cmd_line, start_idx, &idx);
+		else if (cmd_line[idx] == '\'')
+			join_line = parse_quote(cmd_line, start_idx, &idx);
+		else if (cmd_line[idx] == '\"')
+			join_line = parse_double_quote(cmd_line, start_idx, &idx);
+		else
+			continue;
+		temp = ft_strjoin(parsed_line, join_line);
+		free(parsed_line);
+		free(join_line);
+		parsed_line = temp;
+		start_idx = idx + 1;
+	}
+	if (start_idx == idx)
+		return (parsed_line);
+	join_line = ft_strdup_range(cmd_line, start_idx, idx);
 	temp = ft_strjoin(parsed_line, join_line);
 	free(parsed_line);
 	free(join_line);
